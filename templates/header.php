0a6152998b81eeb6e351d3d09606100b1812c64f
Abdul Kholik <akl859349@gmail.com>
2025-10-09 10:06:58 +0700

    update code

--- templates/header.php
+++ templates/header.php
@@ -21,10 +21,8 @@ include_once __DIR__ . '/active_page.php';
     <!-- Font Awesome (CDN fallback if local not present) -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-dymI6X1YQKqj4kNn7cFq8m1qVqf2u7oV3kVX0Qw3Yz5w2d2k4q5Q7lq7w7Zq9x1H1Jff7kYp4Qq3q5q2z5yLw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
-    <!-- Core Theme CSS (SB Admin 2 adapted) -->
-    <link href="<?= $BASE ?>/assets/css/sb-admin-2.css" rel="stylesheet">
-    <link href="<?= $BASE ?>/assets/css/mobile-enhancements.css" rel="stylesheet">
-    <link href="<?= $BASE ?>/assets/css/charts-mobile.css" rel="stylesheet">
+    <!-- Core Theme CSS (SB Admin 2) -->
+    <link href="<?= $BASE ?>/assets/css/sb-admin-2.min.css" rel="stylesheet">
 </head>
 <body id="page-top">
 
